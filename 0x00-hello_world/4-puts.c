a19d9205704907d43294dd67edd10ffd611dd660
“Givted1” <adaspiration@gmail.com>
2023-02-15 22:25:28 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-#INCLUDE <stdio.h>
+#include <stdio.h>
 /**
- * main -A program that prints a line with puts function
- n
- * Return: 0(Success)
- */
+*main - Entry point
+*Return: Always 0 (Success)
+*/
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle
-");
-return (0);
+	puts("\"Programming is like  building a multilingual puzzle");
+		return (0);
 }
