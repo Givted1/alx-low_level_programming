631117af507ce46176062291d3a3e6ec2b8c7cb3
“Givted1” <adaspiration@gmail.com>
2023-02-20 21:16:30 -0800

    2-largest_number.c

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdo.h>
+
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int 1;
+
+	printf("infinite loop incoming :(\n");
+
+	i = 0;
+	/**
+	 *while (i < 10)
+	 *{
+	 *       putchar(i);
+	 *}
+	 */
+	printf("infinite loop avioded! \\0/\n");
+
+	return (0);
+}
