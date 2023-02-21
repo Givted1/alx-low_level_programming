2fd35209fc71dfc0f58fc6bcb9ceaf531d1a7e8e
“Givted1” <adaspiration@gmail.com>
2023-02-21 09:07:57 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,10 +11,11 @@ int main(void)
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
-	_putchar('c');
+	_putchar('C');
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
